5e16dae4b719a740fb9f50508ec98fe62d36807b
Aditya Hargane <adihargane1@gmail.com>
2023-04-29 14:33:57 +0530

    performance page

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
 import Admin from "scenes/admin";
+import Performance from "scenes/performance";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -27,16 +28,17 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="products" element={<Products />} />
-              <Route path="customers" element={<Customers />} />
-              <Route path="transactions" element={<Transactions />} />
-              <Route path="geography" element={<Geography />} />
-              <Route path="overview" element={<Overview />} />
-              <Route path="daily" element={<Daily />} />
-              <Route path="monthly" element={<Monthly />} />
-              <Route path="breakdown" element={<Breakdown />} />
-              <Route path="admin" element={<Admin />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- server/index.js
+++ server/index.js
@@ -18,12 +18,15 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+
 import {
     dataUser,
     dataProduct,
     dataProductStat,
     dataTransaction,
     dataOverallStat,
+    dataAffiliateStat,
 } from "./data/index.js";
 
 // Configuration
@@ -63,5 +66,6 @@ mongoose.connect(process.env.MONGO_URL, {
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
 })
 .catch((error) => console.log(`${error} did not connect`));
